96187adc572aee3661f8288c37b96a196d20b265
dakodun <github@chikin.net>
2015-05-18 23:59:17 +0100

    fixed an error with null data when creating a new vertex from less than four neighbours; commented and added error reporting

--- src/triangulate.cpp
+++ src/triangulate.cpp
@@ -1,6 +1,6 @@
 /* **************************************************************** **
 **	Uair Engine
-**	Copyright (c) 2014 Iain M. Crawford
+**	Copyright (c) 20XX Iain M. Crawford
 **
 **	This software is provided 'as-is', without any express or
 **	implied warranty. In no event will the authors be held liable
@@ -26,6 +26,8 @@
 ** **************************************************************** */
 
 #include "triangulate.hpp"
+
+#include "util.hpp"
 
 namespace uair {
 Triangulate::VertexData::VertexData(const glm::vec2& vertex, const unsigned int& index) {
@@ -90,7 +92,7 @@ Triangulate::~Triangulate() {
 
 Triangulate::Result Triangulate::Process(const WindingRule& windingRule) {
 	if (!mTesselator) {
-		std::cerr << "tesselation error: unable to create tesselator\n";
+		util::LogMessage(1, "tesselation error: unable to create tesselator"); // log the error
 	}
 	
 	mVertices.clear(); // ensure no vertices exist from previous triangulation
@@ -138,53 +140,65 @@ void Triangulate::AddContours(const std::vector< std::vector<glm::vec2> >& conto
 }
 
 void Triangulate::Clear() {
-	mContours.clear();
+	mContours.clear(); // remove any contours added to triangulator
 }
 
 void Triangulate::HandleBegin(GLenum which) {
-	
+	// needed for glutess (unused)
 }
 
 void Triangulate::HandleEnd() {
-	
+	// needed for glutess (unused)
 }
 
 void Triangulate::HandleVertexData(void *vertex_data, void *polygon_data) {
-	Triangulate& triangulate = *(static_cast<Triangulate*>(polygon_data));
-	VertexData& vertex = *(static_cast<VertexData*>(vertex_data));
-	triangulate.mIndices.push_back(vertex.mIndex);
+	Triangulate& triangulate = *(static_cast<Triangulate*>(polygon_data)); // cast instance handle back
+	VertexData& vertex = *(static_cast<VertexData*>(vertex_data)); // cast vertex back
+	triangulate.mIndices.push_back(vertex.mIndex); // add new vertiex's index to instance handle's index array
 }
 
 void Triangulate::HandleCombineData(GLdouble coords[3], void *vertex_data[4], GLfloat weight[4], void **outData, void *polygon_data) {
-	Triangulate& triangulate = *(static_cast<Triangulate*>(polygon_data));
-	std::vector<VertexData*> vertices = {static_cast<VertexData*>(vertex_data[0]), static_cast<VertexData*>(vertex_data[1]),
-			static_cast<VertexData*>(vertex_data[2]), static_cast<VertexData*>(vertex_data[3])};
+	Triangulate& triangulate = *(static_cast<Triangulate*>(polygon_data)); // cast instance handle back
 	
-	Vertex vertex;
-	vertex.mPoint = glm::vec2(coords[0], coords[1]);
-	vertex.mNeighbourIndices = {vertices.at(0)->mIndex, vertices.at(1)->mIndex, vertices.at(2)->mIndex, vertices.at(3)->mIndex};
-	vertex.mNeighbourWeights = {weight[0], weight[1], weight[2], weight[3]};
-	triangulate.mVertices.push_back(std::move(vertex));
+	Vertex vertex; // create a new vertex
+	vertex.mPoint = glm::vec2(coords[0], coords[1]); // set coords of the new vertex
 	
-	VertexData* newVertex = new VertexData(glm::vec2(coords[0], coords[1]), triangulate.mVertexCount);
-	triangulate.mNewVertices.push_back(newVertex);
-	++triangulate.mVertexCount;
-	*outData = newVertex;
+	for (unsigned int i = 0u; i < 4; ++i) { // for all possible neighbours...
+		VertexData* vertexData = static_cast<VertexData*>(vertex_data[i]); // cast the neighbour data back
+		if (vertexData) { // if the neighbour data is valid...
+			vertex.mNeighbourIndices.push_back(vertexData->mIndex); // store the neighbours index
+			vertex.mNeighbourWeights.push_back(weight[i]); // store the neighbours weight (influence on new vertex)
+		}
+	}
+	
+	triangulate.mVertices.push_back(std::move(vertex)); // store new vertex
+	
+	VertexData* newVertex = new VertexData(glm::vec2(coords[0], coords[1]), triangulate.mVertexCount); // create vertex data for the new vertex (cleaned up later)
+	triangulate.mNewVertices.push_back(newVertex); // add new vertex data to store
+	++triangulate.mVertexCount; // increment the total vertex count
+	*outData = newVertex; // set the output to the new vertex data
 }
 
 void Triangulate::HandleErrorData(GLenum errno, void *polygon_data) {
-	std::cerr << "tesselation error: " << gluErrorString(errno) << '\n';
+	const GLubyte* err = gluErrorString(errno); // get error string
+	if (err) { // if error string exists...
+		std::string errString = (const char*)err; // convert the error string
+		util::LogMessage(1, "tesselation error: " + errString); // log the error
+	}
+	else { // otherwise no error string exists...
+		util::LogMessage(1, "tesselation error: unknown error"); // log unknown error
+	}
 }
 
 void Triangulate::HandleEdgeFlag(GLboolean flag) {
-	
+	// needed for glutess (unused)
 }
 
 void Triangulate::FreeNewVertices() {
-	for (auto vertex = mNewVertices.begin(); vertex != mNewVertices.end(); ++vertex) { // for all new vertices...
-		delete *vertex; // delete vertex from heap
+	for (auto vertex = mNewVertices.begin(); vertex != mNewVertices.end(); ++vertex) { // for all new vertex data...
+		delete *vertex; // free vertex data memory
 	}
 	
-	mNewVertices.clear(); // clear new vertex array
+	mNewVertices.clear(); // clear new vertex data array
 }
 }
